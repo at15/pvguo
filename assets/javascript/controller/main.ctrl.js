4c0645a09077fa9fa20291e037755a6554084403
at15 <at15@outlook.com>
2015-04-07 12:52:52 +0800

    Add main ctrl and a poster toggle btn

---
+++ assets/javascript/controller/main.ctrl.js
@@ -0,0 +1,15 @@
+/**
+ * Created by at15 on 15-4-7.
+ */
+angular.module('at15')
+    .controller('MainCtrl', function () {
+        var vm = this;
+
+        vm.noPoster = false;
+
+        vm.togglePoster = togglePoster;
+
+        function togglePoster() {
+            vm.noPoster = !vm.noPoster;
+        }
+    });
